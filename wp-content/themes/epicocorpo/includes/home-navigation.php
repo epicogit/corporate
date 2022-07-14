785b3dae3ea71207af26ecefdd1aa72b0464a46a
We Are Epico <weareepico@gmail.com>
2022-07-14 11:27:08 +0200

    V7 Home

--- wp-content/themes/epicocorpo/includes/home-navigation.php
+++ wp-content/themes/epicocorpo/includes/home-navigation.php
@@ -1,135 +1,234 @@
 <header class="header js-header hidden" data-brightness='dark'>
-		<div class="header__content js-headerContent grid grid--horizontal">
+  <div class="header__content js-headerContent grid grid--horizontal">
+    <div class="header__sacrifice js-headerSacrifice">
+      <div class="row between-xs header__menu">
+        <div class="header__part grid__item">
+          <div class="header-hamburger hamburger">
+            <span class="icon-hamburger">
+              <svg class="svg-icon" viewBox="0 0 74.125 40" enable-background="new 0 0 74.125 40" xml:space="preserve">
+                <use x="0" y="0" xlink:href="#hamburger" />
+              </svg>
+            </span>
+            <span class="icon-close">
+              <svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+                <use x="0" y="0" xlink:href="#cross" />
+              </svg>
+            </span>
+          </div>
+          <div class="header-expanded-view">
+            <span class="header-link header-link--normal link regular upper js-menu-star__link">
+              <a class="no-hover-slant link  no-route italic-hover" data-link="about">ABOUT US </a>
+            </span>
+            <span class="header-link header-link--normal link regular upper js-menu-star__link">
+              <a class="no-hover-slant link  no-route italic-hover" data-link="menu">Benefits</a>
+            </span>
+            <span class="header-link header-link--normal link regular upper js-menu-star__link">
+              <a class="no-hover-slant link  no-route italic-hover" data-link="contact">Come Funziona</a>
+            </span>
+          </div>
+        </div>
+        <div class="header__part grid__item logo">
+          <a href="
+						<?php echo home_url(); ?>" class="no-hover-slant small caps upper bold">
+            <svg class="svg-icon" viewBox="0 0 231.289 40" enable-background="new 0 0 231.289 40" xml:space="preserve">
+              <use x="0" y="0" xlink:href="#logo" />
+            </svg>
+          </a>
+          <a href="
+						<?php echo home_url(); ?>" class="hide--till-s hide--from-l no-hover-slant">
+            <svg class="svg-icon" viewBox="0 0 231.289 40" enable-background="new 0 0 231.289 40" xml:space="preserve">
+              <use x="0" y="0" xlink:href="#logo_bw" />
+            </svg>
+          </a>
+        </div>
+        <div class="header__part grid__item">
+					<div class="header-expanded-view">
 
-			<div class="header__sacrifice js-headerSacrifice">
 
-				<div class="row between-xs header__menu">
-					<div class="header__part grid__item">
-						<div class="header-hamburger hamburger">
-							<span class="icon-hamburger">
-								<svg class="svg-icon" viewBox="0 0 74.125 40" enable-background="new 0 0 74.125 40" xml:space="preserve">
-									<use x="0" y="0" xlink:href="#hamburger" />
-								</svg></span>
-							<span class="icon-close">
-								<svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
-									<use x="0" y="0" xlink:href="#cross" />
-								</svg></span></div>
-						<div class="header-expanded-view">
-							<span class="header-link header-link--normal link regular upper js-menu-star__link"><a class="no-hover-slant link  no-route italic-hover"  data-link="about">ABOUT</a></span>
-							<span class="header-link header-link--normal link regular upper js-menu-star__link"><a class="no-hover-slant link  no-route italic-hover" data-link="menu">MENU</a></span>
-							<span class="header-link header-link--normal link regular upper js-menu-star__link"><a class="no-hover-slant link  no-route italic-hover"  data-link="contact">CONTATTACI</a></span>
-							</div>
+
+
+
+						<span class="header-link header-link--normal link regular upper no-link-to-account " data-section="account">
+										<span data-href="{$BaseHref}myaccount">
+											<svg class="svg-icon" viewBox="0 0 40 35.5" enable-background="new 0 0 40 35.5" xml:space="preserve">
+												<use x="0" y="0" xlink:href="#person" />
+											</svg>
+										</span>
+								</span>
+
+						<span class="header-link header-link--normal link regular upper" data-section="about"> <a class="no-hover-slant" href="{$BaseHref}about"> Contattaci</a> </span>
+
 					</div>
-					<div class="header__part grid__item logo">
-						<a href="<?php echo home_url(); ?>" class="no-hover-slant small caps upper bold">
-              <svg class="svg-icon" viewBox="0 0 231.289 40" enable-background="new 0 0 231.289 40" xml:space="preserve">
-						<use x="0" y="0" xlink:href="#logo" />
-					</svg>
-            </a>
-						<a href="<?php echo home_url(); ?>" class="hide--till-s hide--from-l no-hover-slant">
-              <svg class="svg-icon" viewBox="0 0 231.289 40" enable-background="new 0 0 231.289 40" xml:space="preserve">
-						<use x="0" y="0" xlink:href="#logo_bw" />
-					</svg>
-              </a></div>
-					<div class="header__part grid__item">
-						<div class="header-expanded-view">
+
+					<a href="/myaccount" class="header-link header-link--wishlist link regular upper no-hover-slant">
+						<svg class="svg-icon svg-heart-stroke" viewBox="0 0 40 35.5" enable-background="new 0 0 40 35.5" xml:space="preserve">
+							<use x="0" y="0" xlink:href="#thumbsup" />
+						</svg>
+					</a>
+
+					<a href="" class="header-link header-link--bag link regular upper no-hover-slant" data-section="bag">
+ 						<span class="hide">Lingua</span>
+ 						<span class="header__cartcount js-cartcount cartcount ">
+ 							<span class="cartcount__background"></span>
+ 							<span class="cartcount__number">IT</span>
+ 						</span>
+ 					</a>
 
 
-							<span class="header-link header-link--normal link regular upper ">
-								<a class="no-hover-slant" href="https://www.facebook.com/mykitchenexperience"> <svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
-										<use x="0" y="0" xlink:href="#facebook" />
-									</svg></a>
-							</span>
-							<span class="header-link header-link--normal link regular upper ">
-								<a class="no-hover-slant" href="https://instagram.com/myke_mykitchenexperience"><svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
-										<use x="0" y="0" xlink:href="#instagram" />
-									</svg></a>
-							</span>
-							<!--<span class="header-link header-link--normal link regular upper ">
-								<a class="no-hover-slant" href="https://www.tripadvisor.it/Restaurant_Review-g187849-d4242116-Reviews-Myke-Milan_Lombardy.html"><svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
-										<use x="0" y="0" xlink:href="#tripadvisor" />
-									</svg></a>
-							</span>-->
+        </div>
+      </div>
+      <div class="header__bottom-edge hide">
+        <div class="header-loading">
+          <span class="header-loading__line 3"></span>
+          <span class="header-loading__line 4"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="menu__container js-menu">
+    <div class="menu allow-touch-scroll">
+      <div class="menu__part row center-xs">
+        <div class="menu__part__inner">
+          <div class="menu__header grid--green grid__item col-xs-12 row middle-xs">
+            <div class="col-xs-12 menu__header__content">
+              <h2 class="h1 mobile-menu__heading">
+								<svg class="svg-icon" viewBox="0 0 231.289 40" enable-background="new 0 0 231.289 40" xml:space="preserve">
+		              <use x="0" y="0" xlink:href="#logo" />
+		            </svg>
+              </h2>
+              <div class="menu-breadcrumbs js-menu-breadcrumbs product-hero__breadcrumb no-margin"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="menu__part menu__items menu-links">
+        <div class="menu__part__inner row">
+          <div class="col-xs-12">
+            <ul class="col-xs-12 menu-links__list">
+              <li class="menu-links__item grid__item col-xs-12 grid--padding js-menu-star__link ">
+                <a data-link="about" class="menu-links__link no-route link upper"> About Us </a>
+              </li>
+              <li class="menu-links__item grid__item col-xs-12 grid--padding js-menu-star__link ">
+                <a data-link="menu" class="menu-links__link no-route link upper"> Benefits </a>
+              </li>
+              <li class="menu-links__item grid__item col-xs-12 grid--padding js-menu-star__link">
+                <a data-link="contact" class="menu-links__link no-route link upper"> Come funziona </a>
+              </li>
+            </ul>
+          </div>
+          <div class="menu-infos col-xs-12">
+            <div class="menu-info__part grid--padding">
+              <p class="small bold">Epico Play s.r.l.</p>
+              <ul class="small">
+                <li>Lecce</li>
+                <li>
+                  <a href="https://goo.gl/maps/FBJXkGFGxj4CugsEA">Via duca d'aosta 19, 73100</a>
+                </li>
+                <li>
+                  <a href="tel:+390332731394" class="chat no-route">0332 731394</a>
+                </li>
+              </ul>
+            </div>
+            <div class="menu-info__part grid--padding">
+              <p class="small bold">Contattaci</p>
+              <ul class="small">
+                <li>email</li>
+                <li>
+                  <a href="https://goo.gl/maps/XQNRYZrZ48cTfWBz6">Whatsup</a>
+                </li>
+                <li>
+                  <a href="tel:+390236741567" class="chat no-route">02 36741567</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+		<div class="menu-sections__container allow-touch-scroll js-menuSectionContainer">
 
-
-						</div>
-
-
-					</div>
-				</div>
-				<div class="header__bottom-edge hide">
-					<div class="header-loading">
-						<span class="header-loading__line 3"></span>
-						<span class="header-loading__line 4"></span></div>
-				</div>
+			<div class="menu-section is-mobile-closed menu-section--right menu-section--account border--radius-bottom-left border--radius-bottom-right" data-section="account" data-component="login">
+<div class="row end-xs">
+	<form enctype="application/x-www-form-urlencoded" method="post" action="Security/LoginForm" id="MemberLoginForm_LoginForm" class="grid__item grid--padding categories js-login-form">
+		<div class="row center-xs">
+			<div class="menu-section__header grid__item col-xs-12 grid--padding">
+				<h2 class="h1 text--dark">Account</h2>
 			</div>
 		</div>
 
-		<div class="menu__container js-menu">
-			<div class="menu allow-touch-scroll">
-				<div class="menu__part row center-xs">
-					<div class="menu__part__inner">
-						<div class="menu__header grid--green grid__item col-xs-12 row middle-xs">
-							<div class="col-xs-12 menu__header__content">
-								<h2 class="h1 mobile-menu__heading"><img src="/wp-content/uploads/2020/12/logo_home.png" alt="" style="max-width: 100px;text-align: center;" /></h2>
-								<div class="menu-breadcrumbs js-menu-breadcrumbs product-hero__breadcrumb no-margin">
+		<div class="row menu-section__items start-xs">
+			<div class="grid__item col-xs-12 grid--no-edge"><span class="error-message ui-error-message" id="MemberLoginForm_LoginForm_error">
+								</span>
 
-								</div>
-							</div>
-						</div>
+				<div class="input__container input__container--text border--radius-medium   ">
+					<div class="input__input input-theme--bordered border--radius-medium" id="Email">
+						<input type="text" value="" name="Email" id="MemberLoginForm_LoginForm_Email" class="input-text" required  placeholder="Email"><span class="check--svg">
+							<svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+								<use x="0" y="0" xlink:href="#check" />
+							</svg>
+						</span>
+
+						<div class="input__status input__status--error"></div>
+
+						<div class="input__status input__status--valid"></div>
+
+						<label for="MemberLoginForm_LoginForm_Email">Email</label>
 					</div>
 				</div>
-				<div class="menu__part menu__items menu-links">
-					<div class="menu__part__inner row">
-						<div class="col-xs-12">
 
-							<ul class="col-xs-12 menu-links__list">
-								<li class="menu-links__item grid__item col-xs-12 grid--padding js-menu-star__link " >
-									<a data-link="about" class="menu-links__link no-route link upper"> About </a></li>
-								<li class="menu-links__item grid__item col-xs-12 grid--padding js-menu-star__link "><a data-link="menu" class="menu-links__link no-route link upper"> Menu </a></li>
-								<li class="menu-links__item grid__item col-xs-12 grid--padding js-menu-star__link" >
-									<a data-link="contact" class="menu-links__link no-route link upper"> Contact </a></li>
+				<div class="input__container input__container--text  border--radius-medium  ">
+					<div class="input__input input-theme--bordered border--radius-medium" id="Password">
+						<input type="password" value="" name="Password" id="MemberLoginForm_LoginForm_Password" class="input-text" required  placeholder="Password"><span class="check--svg">
+							<svg class="svg-icon" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+								<use x="0" y="0" xlink:href="#check" />
+							</svg>
+						</span>
 
-							</ul>
-						</div>
-						<div class="menu-infos col-xs-12">
-							<div class="menu-info__part grid--padding">
-								<p class="small bold">Myke Burger</p>
-								<ul class="small">
+						<div class="input__status input__status--error"></div>
 
-									<li>Gavirate c/o C.C. </li>
-									<li><a href="https://goo.gl/maps/FBJXkGFGxj4CugsEA">Campo dei Fiori Gavirate (VA)</a></li>
-									<li><a href="tel:+390332731394" class="chat no-route">0332 731394</a></li>
-								</ul>
-							</div>
-							<div class="menu-info__part grid--padding">
-								<p class="small bold">Myke Lab</p>
-								<ul class="small">
-									<li>Milano</li>
-									<li><a href="https://goo.gl/maps/XQNRYZrZ48cTfWBz6">Via M. Quadrio 25</a></li>
-									<li><a href="tel:+390236741567" class="chat no-route">02 36741567</a></li>
-								</ul>
-							</div>
-						</div>
+						<div class="input__status input__status--valid"></div>
+
+						<label for="MemberLoginForm_LoginForm_Password">Password</label>
 					</div>
 				</div>
-			</div>
 
-
-			<script id="menuFeaturedBookTemplate" type="text/html">
-			<a href="<%= url %>" class="row center-xs no-hover-slant menu-section-book-highlight__inner js-headerFeaturedInner">
-				<p class="menu-section-highlight__heading label upper">Highlight</p>
-				<div class="js-headerFeaturedContentChange" style="">
-					<div class="menu-section-book-highlight__image js-headerFeaturedImage image">
-						<img src="<%= image %>" class="menu-section-book-highlight__image">
-					</div>
-					<p class="menu-section-book-highlight__title hover-italic"><%= title %></p>
-					<p class="price">€ <%= price %></p>
-				</div>
-			</a>
-			</script>
+											<a href="Security/lostpassword" class="small text--dark">Password dimenticata? </a></div>
 		</div>
-		<script id="breadcrumbsTemplate" type="text/html">
-		<ul class="menu-breadcrumbs__list"> <% _.each(paths, function(item, i){ %> <% if (i !== 0) { %> <span class="icon arrow-right"></span> <% } %> <p class="link upper no-margin text--light"><%= item %></p> <% }); %> </ul>
-		</script>
-	</header>
+
+		<div class="row middle-xs between-xs grid--padding--half-top ">
+			<div class=" col-xs-6 start-xs">
+				<!--<a href="$FacebookLoginLink" class="link upper regular text--dark"><% if Locale == 'it_IT' %>Iscriviti con facebook<% else %>Sign up with facebook<% end_if %></a>-->
+				<a href="{$BaseHref}dashboard?locale=$Locale" class="button   light--shadow  border--radius-small button--white small caps spaced   regular text--dark">Iscriviti</a></div>
+
+			<div class=" col-xs-6">
+			<button type="submit" class="button   light--shadow button--download border--radius-small small caps spaced prevent-default" >Accedi
+					<span class="button-hover"><span><span class="buy-msg">Accedi</span></span></span></button>
+			</div>
+		</div>
+
+		<input type="hidden" value="MemberAuthenticator" name="AuthenticationMethod" id="MemberLoginForm_LoginForm_AuthenticationMethod" class="hidden">
+	</form>
+</div>
+</div>
+
+
+		</div>
+
+
+    <script id="menuFeaturedBookTemplate" type="text/html">
+      <a href="<%= url %>" class="row center-xs no-hover-slant menu-section-book-highlight__inner js-headerFeaturedInner">
+        <p class="menu-section-highlight__heading label upper">Highlight</p>
+        <div class="js-headerFeaturedContentChange" style="">
+          <div class="menu-section-book-highlight__image js-headerFeaturedImage image">
+            <img src="<%= image %>" class="menu-section-book-highlight__image">
+          </div>
+          <p class="menu-section-book-highlight__title hover-italic"><%= title %> </p>
+          <p class="price">€ <%= price %> </p>
+        </div>
+      </a>
+    </script>
+  </div>
+  <script id="breadcrumbsTemplate" type="text/html">
+    <ul class="menu-breadcrumbs__list"><% _.each(paths, function(item, i){ %><% if (i !== 0) { %> <span class="icon arrow-right"></span><% } %> <p class="link upper no-margin text--light"><%= item %> </p><% }); %> </ul>
+  </script>
+</header>
